784f3d646befba260b9c54cb66992bb8b67ae6dd
rahmi45 <rachid.rahmi@gmail.com>
2022-02-06 13:36:43 +0100

    Init Commit : Basic structure and simple project Spring Boot

---
+++ src/main/java/com/spring/SpringInitApplication.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringInitApplication.class, args);
+	}
+
+}
